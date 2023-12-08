8f3dcbbfeb61fe73db39a586ddf19dd8c930cd9c
Mitchell Bricco <briccm84@uwosh.edu>
2023-12-08 08:48:08 -0600

    moved shapes creation to seperate file

--- lab9.js
+++ lab9.js
@@ -49,8 +49,7 @@ let materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
  //metallic?
 let materialShininess = 100.0;          
 
-
-
+let shapes;
 let nf;
 
 //global variables to help us with vertex array objects
@@ -111,69 +110,7 @@ function init(){
 	program = initShaders( gl, "vertex-shader", "fragment-shader" );
 	gl.useProgram( program );
 
-    let shape = createSphereVertices(.25,50,50);
-    let sphere = {
-        shape: shape,
-        vao: setUpVertexObject(shape),
-        translation: translate(-.6,.3,0),
-        materialDiffuse:  vec4(  0.15,0.96, 0.17, 1.0), 
-        materialAmbient:  vec4( 1.0, 1.0, 1.0, 1.0 ),
-        materialSpecular: vec4( 1.0, 1.0, 1.0, 1.0 ),
-        materialShininess: 400.0,
-    };
-    shape = createTruncatedConeVertices(.2,0,1,50,50);
-    let cone = {
-        shape: shape,
-        vao: setUpVertexObject(shape),
-        translation: translate(.7,-.5,0),
-        materialDiffuse:  vec4( 0.2, 0.2, 1.0, 1.0), 
-        materialAmbient:  vec4( 1.0, 1.0, 1.0, 1.0 ),
-        materialSpecular: vec4( 1.0, 1.0, 1.0, 1.0 ),
-        materialShininess: 100.0,
-    };
-    shape = createCubeVertices(.4);
-    let cube = {
-        shape: shape,
-        vao: setUpVertexObject(shape),
-        translation: translate(-.6,-.5,0),
-        materialDiffuse:  vec4( 0.9, 0.9, 0.2, 1.0), 
-        materialAmbient:  vec4( 1.0, 1.0, 1.0, 1.0 ),
-        materialSpecular: vec4( 1.0, 1.0, 1.0, 1.0 ),
-        materialShininess: 100.0,
-    };
-    shape = createTruncatedConeVertices(.125,.125,.75,50,50);
-    let cylinder = {
-        shape: shape,
-        vao: setUpVertexObject(shape),
-        translation: translate(0,-.4,0),
-        materialDiffuse:  vec4( 0.95, 0.2, 0.2, 1.0), 
-        materialAmbient:  vec4( 1.0, 1.0, 1.0, 1.0 ),
-        materialSpecular: vec4( 0.4, 0.4, 0.4, 1.0 ),
-        materialShininess: 400.0,
-    };
-    //You will not use a mesh but parametric shapes                         for ghost
-    let coords = ghost.vertices[0].values;
-    let indices = ghost.connectivity[0].indices;
-    //Normals provided by Meshlab in json file
-    let normals = ghost.vertices[1].values;
-    let texcoords = 0;
-    shape = {
-        positions: coords,
-        normals: normals,
-        texcoord: texcoords,
-        indices: indices,
-    };
-    let Ghost1 = {
-        shape: shape,
-        vao: setUpVertexObject(shape),
-        translation: mult(translate(0,.4,0),scalem(.1,.1,-0.1)),
-        materialDiffuse:  vec4( 0.95, 0.2, 0.2, 1.0), 
-        materialAmbient:  vec4( 1.0, 1.0, 1.0, 1.0 ),
-        materialSpecular: vec4( 0.4, 0.4, 0.4, 1.0 ),
-        materialShininess: 400.0,
-    }
-    
-    shapes = [sphere,cone,cube,cylinder,Ghost1];
+    shapes = getShapes();
         
     //set up uniform variables
     uniformModelView = gl.getUniformLocation(program, "u_modelViewMatrix");
